b927f2259fb01c159e5fa2c67b66424bd1582780
abduzeedo <fabio.sasso@gmail.com>
2022-09-05 17:48:21 -0700

    Preview of new abdz

---
+++ labs/abdz-22/abdz_files/ajax-throbber.js
@@ -0,0 +1,81 @@
+(function ($, window, Drupal, drupalSettings) {
+
+  'use strict';
+  /**
+   * Prepare the Ajax request before it is sent.
+   *
+   * @param {XMLHttpRequest} xmlhttprequest
+   * @param {object} options
+   * @param {object} options.extraData
+   */
+  Drupal.Ajax.prototype.beforeSend = function (xmlhttprequest, options) {
+    // For forms without file inputs, the jQuery Form plugin serializes the
+    // form values, and then calls jQuery's $.ajax() function, which invokes
+    // this handler. In this circumstance, options.extraData is never used. For
+    // forms with file inputs, the jQuery Form plugin uses the browser's normal
+    // form submission mechanism, but captures the response in a hidden IFRAME.
+    // In this circumstance, it calls this handler first, and then appends
+    // hidden fields to the form to submit the values in options.extraData.
+    // There is no simple way to know which submission mechanism will be used,
+    // so we add to extraData regardless, and allow it to be ignored in the
+    // former case.
+    if (this.$form) {
+      options.extraData = options.extraData || {};
+
+      // Let the server know when the IFRAME submission mechanism is used. The
+      // server can use this information to wrap the JSON response in a
+      // TEXTAREA, as per http://jquery.malsup.com/form/#file-upload.
+      options.extraData.ajax_iframe_upload = '1';
+
+      // The triggering element is about to be disabled (see below), but if it
+      // contains a value (e.g., a checkbox, textfield, select, etc.), ensure
+      // that value is included in the submission. As per above, submissions
+      // that use $.ajax() are already serialized prior to the element being
+      // disabled, so this is only needed for IFRAME submissions.
+      var v = $.fieldValue(this.element);
+      if (v !== null) {
+        options.extraData[this.element.name] = v;
+      }
+    }
+
+    // Disable the element that received the change to prevent user interface
+    // interaction while the Ajax request is in progress. ajax.ajaxing prevents
+    // the element from triggering a new request, but does not prevent the user
+    // from changing its value.
+    $(this.element).prop('disabled', true);
+
+    if (!this.progress || !this.progress.type) {
+      return;
+    }
+
+    // Insert progress indicator.
+    if (this.progress.type == 'throbber' && drupalSettings.ajaxLoader.alwaysFullscreen) {
+      // Always show throbber as fullscreen overlay.
+      this.progress.type = 'fullscreen';
+    }
+    var progressIndicatorMethod = 'setProgressIndicator' + this.progress.type.slice(0, 1).toUpperCase() + this.progress.type.slice(1).toLowerCase();
+    if (progressIndicatorMethod in this && typeof this[progressIndicatorMethod] === 'function') {
+      this[progressIndicatorMethod].call(this);
+    }
+  };
+
+  /**
+   * Overrides the throbber progress indicator.
+   */
+  Drupal.Ajax.prototype.setProgressIndicatorThrobber = function () {
+    this.progress.element = $('<div class="ajax-progress ajax-progress-throbber"><div class="ajax-loader">' + drupalSettings.ajaxLoader.markup + '</div></div>');
+    if (this.progress.message && !drupalSettings.ajaxLoader.hideAjaxMessage) {
+      this.progress.element.find('.ajax-loader').after('<div class="message">' + this.progress.message + '</div>');
+    }
+    $(this.element).after(this.progress.element);
+  };
+
+  /**
+   * Sets the fullscreen progress indicator.
+   */
+  Drupal.Ajax.prototype.setProgressIndicatorFullscreen = function () {
+    this.progress.element = $('<div class="ajax-progress ajax-progress-fullscreen">' + drupalSettings.ajaxLoader.markup + '</div>');
+    $(drupalSettings.ajaxLoader.throbberPosition).after(this.progress.element);
+  };
+
+})(jQuery, this, Drupal, drupalSettings);
---
+++ labs/abdz-22/abdz_files/main.js
@@ -0,0 +1,170 @@
+var imgs = []
+var colors = []
+var shadows = []
+var curve = 'cubic-bezier(.25,.1,.39,1.4)'
+var submenuOn = false
+
+//  window.sr = ScrollReveal();
+;(function ($) {
+  Drupal.behaviors.myModuleBehavior = {
+    attach: function (context, settings) {
+    //   //  jQuery once ensures that code does not run after an AJAX or other function that calls Drupal.attachBehaviors().
+    //   $('body')
+    //     .once('abdz')
+    //     .each(function () {
+    //       // We have console.log() here to make it easy to see that this code is functioning. You should never use console.log() on production code!
+    //       // if (typeof console.log === 'function') {
+    //       //   console.log('My Setting: ' + settings.sampleLibrary.mySetting);
+    //       // }
+    //     })
+    //   if (typeof console.log === 'function') {
+    //     //console.log('This will run every time Drupal.attachBehaviors is run.');
+    //   }
+    //   $('body')
+    //     .once('abdzModifyDOM')
+    //     .each(function () {
+    //       // Add an element to the body.
+    //       //  $('body').append('<div class="example">Hello World</div>');
+    //       //console.log('Modify DOM');
+    //       // Tell Drupal that we modified the DOM.
+    //       Drupal.attachBehaviors()
+    //     })
+      replaceImgs()
+      changeTheme()
+      //imgGallery()
+      removeEmptyPs()
+      gliteboxImgs()
+      //$("#sidebar .content").height($(document).height())
+      var sidebar_open = false
+      $('header .close_menu').click(function(){
+          if (sidebar_open == false){
+            sidebar(0,"-100vw")
+            $("body").addClass("fixed")
+            $(".close_menu").addClass("closed")
+            $(".cc-revoke").hide()
+            gsap.to('.close_menu', { rotate: 360, duration: .3, ease: 'expo.out' })
+            sidebar_open = true
+          }
+          else{
+            sidebar("100vw",0)
+            $("body").removeClass("fixed")
+            $(".close_menu").removeClass("closed")
+            $(".cc-revoke").show()
+            gsap.to('.close_menu', { rotate: 0, duration: .3, ease: 'expo.out' })
+            sidebar_open = false
+          }
+      })
+    },
+  }
+  function sidebar(foo, boo,){
+    gsap.to('#sidebar', {
+        x: foo,
+        duration: 1,
+        ease: 'expo.out',
+      })
+      gsap.to('main', { x: boo, duration: 1, ease: 'expo.out' })
+  }
+  function removeEmptyPs() {
+    $('.blog-content p').each(function () {
+      var $p = $(this),
+        txt = $p.html()
+      if (txt == '&nbsp;') {
+        $p.remove()
+      }
+    })
+  }
+  window.onscroll = function () {
+
+  }
+  function imgGallery() {
+    if ($('.imgsGrid')) {
+      $('.imgsGrid').photosetGrid({
+        gutter: '4px',
+        layout: '1212121212121212121212121212121212121212121212121212',
+      })
+    }
+    if ($('.photoset-grid')) {
+      $('.photoset-grid').photosetGrid({
+        gutter: '4px',
+      })
+    }
+  }
+  function changeTheme() {
+    var date = new Date()
+    var hr = date.getHours()
+    if (hr > 5 && hr < 19) {
+      $('html').removeClass('night')
+    }
+    if (hr >= 19) {
+      $('html').addClass('night')
+    }
+    if (hr >= 0 && hr <= 5) {
+      $('html').addClass('night')
+    }
+  }
+
+  function collectionsMenu() {
+    $(
+      '.menu-main li:nth-child(1) a , .mobile_menu li:nth-child(0) a',
+    ).removeAttr('href')
+    $('.menu-main li:nth-child(1) a , .mobile_menu li:nth-child(0) a').click(
+      function () {
+        if (submenuOn) {
+          $('.submenu-collections').hide()
+          setTimeout(function () {
+            submenuOn = false
+          }, 100)
+        } else {
+          //$("main .posts").hide()
+          $('.submenu-collections').show()
+          setTimeout(function () {
+            submenuOn = true
+          }, 100)
+        }
+      },
+    )
+  }
+  function replaceImgs() {
+    $(".path-frontpage article.post-item").each(function(i){
+        //console.log("Test " + i)
+        placeholder = $(this).find('div.hero_image a')
+        img = $(this).find('div.hero_image picture img')
+        imgSrc = img.prop('src')
+        //imgs.push(imgSrc)
+        backgroundImg = 'url(' + imgSrc + ') no-repeat center center'
+        placeholder.css('background', backgroundImg)
+        placeholder.css('background-size', 'cover')
+    })
+   }
+
+   function gliteboxImgs(){
+    $(".blog-post img").each(function(i){
+     img = $(this)
+     //img.removeAttr("alt")
+     img.attr('data-type',"image")
+     img.attr('data-description',"")
+     img.addClass("glightbox")
+    })
+    const lightbox = GLightbox({
+        touchNavigation: true,
+        loop: true,
+    });
+  }
+
+
+  document.addEventListener('keydown', function (event) {
+    if (event.code == 'KeyN') {
+      if ($('html').hasClass('night')) {
+        cc = setTimeout(function () {
+          $('html').removeClass('night')
+          clearTimeout(cc)
+        }, 100)
+      } else {
+        cc = setTimeout(function () {
+          $('html').addClass('night')
+          clearTimeout(cc)
+        }, 100)
+      }
+    }
+  })
+})(jQuery)
