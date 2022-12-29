3981b6adb038b9c556e80a861482a0997b2517d9
abduzeedo <fabio.sasso@gmail.com>
2022-12-29 09:29:19 -0800

    Update index.js

--- 2022/d/index.js
+++ 2022/d/index.js
@@ -2,7 +2,7 @@ window.onload = function(){
     let vh = window.innerHeight
     document.documentElement.style.setProperty('--window-height', `${vh}px`);
   };
-  window.onresize = function(event) {
-    let vh = window.innerHeight
-    document.documentElement.style.setProperty('--window-height', `${vh}px`);
-}
+//   window.onresize = function(event) {
+//     let vh = window.innerHeight
+//     document.documentElement.style.setProperty('--window-height', `${vh}px`);
+// }
