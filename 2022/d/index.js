d84489e198954b5b83a12d6f673ad89aa36779d1
abduzeedo <fabio.sasso@gmail.com>
2022-12-30 13:36:06 -0800

    Window adjustments

--- 2022/d/index.js
+++ 2022/d/index.js
@@ -1,8 +1,13 @@
 window.onload = function(){
-    let vh = window.innerHeight
-    document.documentElement.style.setProperty('--window-height', `${vh}px`);
-  };
-//   window.onresize = function(event) {
-//     let vh = window.innerHeight
-//     document.documentElement.style.setProperty('--window-height', `${vh}px`);
-// }
+   getWindowHeight()
+};
+window.onresize = function(event) {
+  getWindowHeight()  
+}
+
+function getWindowHeight(){
+  let vh = window.innerHeight
+  let vw = window.innerWidth
+  document.documentElement.style.setProperty('--window-height', `${vh}px`);
+  document.getElementById("vh").innerText = `width: ${vw}px & height: ${vh}px`
+}
