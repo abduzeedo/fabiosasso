13b992bddb5268a99d228cf2e99542509a025fa2
abduzeedo <fabio.sasso@gmail.com>
2023-01-02 20:15:45 -0800

    Update index.js

--- 2022/d/index.js
+++ 2022/d/index.js
@@ -2,13 +2,18 @@ window.onload = function(){
    getWindowHeight()
 };
 window.onresize = function(event) {
-  //getWindowHeight()  
+  getWindowHeight()  
 }
 
 function getWindowHeight(){
   let vh = window.innerHeight
   let vw = window.innerWidth
-  document.documentElement.style.setProperty('--window-height', `${vh}px`);
+  if(vh > 767){
+    document.documentElement.style.setProperty('--window-height', `${vh}px`);
+  }
+  if(vh == 775){
+    document.documentElement.style.setProperty('--window-height', `774px`);
+  }
   document.getElementById("vh").innerText = `width: ${vw}px & height: ${vh}px`
 
 }
