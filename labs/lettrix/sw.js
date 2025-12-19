307f3b2e78e68967c39c13f3adf3be7be9cbf6cf
abduzeedo <fabio.sasso@gmail.com>
2025-12-18 19:44:07 -0800

    Lettrix game

---
+++ labs/lettrix/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'letris-v2';
+const ASSETS = [
+  './letris.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
